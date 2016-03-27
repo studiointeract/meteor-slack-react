b5efb6ee52ba10f0057a2392793ffc8c1fa73004
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 23:58:02 +0200

    Improving readability.

--- imports/ui/components/ChannelMessages.jsx
+++ imports/ui/components/ChannelMessages.jsx
@@ -7,7 +7,9 @@ class ChannelMessages extends Tracker.Component {
   constructor(props) {
     super(props);
     this.autorun(() => this.setState({
-      messages: Messages.find({ channel: this.props.channel }).fetch()
+      messages: Messages.find({
+        channel: this.props.channel
+      }).fetch()
     }));
   }
 
