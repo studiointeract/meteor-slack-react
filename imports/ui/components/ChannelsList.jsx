7931efed460407139c8a448bfa7998b054f86015
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 20:35:15 +0200

    Added missing key on channels list.

--- imports/ui/components/ChannelsList.jsx
+++ imports/ui/components/ChannelsList.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ChannelsList = ({ channels, active, handleSelect }) => (
   <ul className="channels">
-    {channels.map(channel =>
-       <li onClick={handleSelect}>
+    {channels.map((channel, i) =>
+       <li onClick={handleSelect} key={i}>
          <a href={`/${channel._id}`}
             className={channel._id == active ? "active" : ""}>{channel.name}</a>
        </li>
