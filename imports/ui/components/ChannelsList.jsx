053ed54bffa65e37d19b05998144f627c7458950
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 21:18:36 +0200

    Simplifying channelslist.

--- imports/ui/components/ChannelsList.jsx
+++ imports/ui/components/ChannelsList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ChannelsList = ({ channels, active, handleSelect }) => (
+const ChannelsList = ({ channels, active }) => (
   <ul className="channels">
     {channels.map((channel, i) =>
-       <li onClick={handleSelect} key={i}>
+       <li key={i}>
          <a href={`/${channel._id}`}
             className={channel._id == active ? "active" : ""}>{channel.name}</a>
        </li>
