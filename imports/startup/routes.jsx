514c37f13a12d46dd10fc9e30b4c431247eba131
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 20:25:30 +0200

    Step 6.

--- imports/startup/routes.jsx
+++ imports/startup/routes.jsx
@@ -2,9 +2,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router-ssr';
 import React from 'react';
 import {mount} from 'react-mount-layout';
 import MainLayout from '../ui/layouts/MainLayout.jsx';
+import Navigation from '../ui/components/Navigation.jsx';
 
 FlowRouter.route("/", {
   action() {
-    mount(MainLayout, {content: "Connecting ..."});
+    mount(MainLayout, {
+      nav: <Navigation />,
+      content: "Connecting ..."
+    });
   }
 });
