30857820d36481e4073463b17ec6892518f9048b
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 21:10:38 +0200

    Step 8.

--- imports/startup/routes.jsx
+++ imports/startup/routes.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 import {mount} from 'react-mount-layout';
 import MainLayout from '../ui/layouts/MainLayout.jsx';
 import Navigation from '../ui/components/Navigation.jsx';
+import ChannelPage from '../ui/pages/ChannelPage.jsx';
 
 FlowRouter.route("/", {
   action() {
     mount(MainLayout, {
       nav: <Navigation />,
       content: "Connecting ..."
+    });
+  }
+});
+
+FlowRouter.route("/:channel", {
+  action(params) {
+    mount(MainLayout, {
+      nav: <Navigation />,
+      content: <ChannelPage channel={params.channel} />
     });
   }
 });
---
+++ imports/ui/pages/ChannelPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracker from 'tracker-component';
+
+const ChannelPage = ({ channel }) => (
+  <div className="channel">
+    <header>
+      <h1>#{channel}</h1>
+    </header>
+  </div>
+);
+
+export default ChannelPage;
