c9810677bc386e3e4d38dc8b4cef963d91250004
Tim Brandin <tim.brandin@gmail.com>
2016-03-27 20:34:12 +0200

    Step 7.

--- server/main.js
+++ server/main.js
@@ -1,1 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+
+// This defines a starting set of data to be loaded if the app is loaded with an empty db.
+import '/imports/startup/server/fixtures.js';
+
+// This defines all the collections, publications and methods that the application provides
+// as an API to the client.
+import '/imports/api';
+
+// This will give you Server Side Rendering from FlowRouter SSR.
+import '/imports/startup/routes.jsx';
